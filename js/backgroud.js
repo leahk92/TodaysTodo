382875a3083ba4b69110e8eb992e408a480e0124
leahk92 <164601321+leahk92@users.noreply.github.com>
2024-07-11 12:20:08 +0900

    backgroud.js

--- js/backgroud.js
+++ js/backgroud.js
@@ -1,9 +1,5 @@
-const images = [
-    "mountain.jpg", "cactus.jpg", "Albuquerque.jpg", "mountain2.jpg", "mountain3.jpg",
-    "sheep1.jpg", "sheep2.jpg", "cow1.jpg", "desert1.jpg", "penguin1.jpg",
-    "sky1.jpg", "cat1.jpg", "cat2.jpg", "cat3.jpg", "cat4.jpg", "cat6.jpg", "cat7.jpg"
-];
+const images = ["mountain.jpg", "cactus.jpg", "Albuquerque.jpg", "mountain2.jpg", "mountain3.jpg", "sheep1.jpg", "sheep2.jpg", "cow1.jpg", "desert1.jpg", "penguin1.jpg", "sky1.jpg", "cat1.jpg", "cat2.jpg", "cat3.jpg", "cat4.jpg", "cat6.jpg", "cat7.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url('../img/${chosenImage}')`;
+document.body.style.backgroundImage = `url("../img/${chosenImage}")`;
